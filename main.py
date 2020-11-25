1036c3d89023ef1ee8916a6684928ff078da0b3a
luma24 <57962904+luma24@users.noreply.github.com>
2020-11-24 22:11:07 -0800

    Update main.py
    This code ran successfully, and I got 4 successful replies from my EC2 Instance!

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # This is the template code for the CNA337 Final Project
-# Zachary Rubin, zrubin@rtc.edu
-# CNA 337 Fall 2020
+# Luma Naser, lnaser@student.rtc.edu
+# CNA 337 Fall 11/24/2020
+
+from Server import Server
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Luma")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
+    EC2server = Server('3.17.16.187')
     # TODO - Call Ping method and print the results
+    print(EC2server.ping())
